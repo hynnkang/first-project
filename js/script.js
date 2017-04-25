6d212efe771457784dcebe46988e24d21dbadc17
sarahKangHY <ganghy6@gmail.com>
2017-04-25 21:04:03 +0900

    example website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
-  $('#TopSlider .slide').bxSlider(
+  $('#TopSlider .slide').bxSlider();
     
-  );
+    $('#nav .menubox').hide();
     
-});
+    $('#new .all h3').addClass('active');
+    $('#new h3').on('click',function(){
+        $('#new .item').css('display', 'none');
+        $(this).siblings('#new .item').css('display', 'block');
+        $('#new h3').removeClass('active');
+        $(this).addClass('active');
+    });
+    
+    
+    $('#nav .menuname a').on('mouseenter',function(){
+        $('#nav .menubox').slideDown();
+        
+    });
+    
+    
+    $(window).scroll(function(){
+        
+        if ($(document).scrollTop() >= 700 ) {
+        
+        $('#nav').css('display','fixed');
+    }
+        
+    });
+                     
+    
+
+    });
